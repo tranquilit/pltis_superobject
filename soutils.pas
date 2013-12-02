922b884bcf2f639b11a3276bee9e440aa4e3d0ee
htouvet <htouvet-github@tranquil.it>
2013-12-02 21:02:17 +0100

    cs2SSO function

--- soutils.pas
+++ soutils.pas
@@ -5,7 +5,7 @@ unit soutils;
 interface
 
 uses
-  Classes, SysUtils,SuperObject,DB;
+  Classes, SysUtils,SuperObject,DB,clipbrd,lcltype;
 
 function StringList2SuperObject(St:TStringList):ISuperObject;
 function SplitLines(const St:String):ISuperObject;
@@ -19,6 +19,13 @@ function StrToken(var S: string; Separator: Char): string;
 
 function Dataset2SO(DS:TDataset;AllRecords:Boolean=True):ISuperObject;
 procedure SO2Dataset(SO:ISuperObject;DS:TDataset;ExcludedFields:Array of String);
+
+function csv2SO(csv:UTF8String;Sep:Char=#0):ISUperObject;
+
+function ClipboardSOData: ISuperObject;
+
+var
+  ClipbrdJson: TClipboardFormat;
 
 implementation
 uses StrUtils,character,superdate;
@@ -231,7 +238,76 @@ begin
   end;
 end;
 
+function csv2SO(csv: UTF8String;Sep:Char=#0): ISUperObject;
+var
+  r,col,maxcol:integer;
+  row : String;
+  Lines,header,values,newrec:ISuperObject;
+begin
+  lines := SplitLines(csv);
+  row := lines.AsArray.S[0];
+  if Sep=#0 then
+  begin
+    if pos(#9,row)>0 then
+      Sep := #9
+    else
+    if pos(';',row)>0 then
+      Sep := ';'
+    else
+    if pos(',',row)>0 then
+      Sep := ',';
+  end;
 
+  header := Split(row,Sep);
+  result := TSuperObject.Create(stArray);
+  if Lines.AsArray.Length>1 then
+    for r:=1 to lines.AsArray.Length-1 do
+    begin
+      row := lines.AsArray.S[r];
+      values :=Split(row,sep);
+      Newrec := TSuperObject.Create;
+      result.AsArray.Add(newrec);
+      maxcol := values.AsArray.Length;
+      if maxcol > header.AsArray.Length then
+        maxcol := header.AsArray.Length;
+
+      for col := 0 to maxcol-1 do
+        newrec.S[header.AsArray.S[col]] := UTF8Decode(values.AsArray.S[col]);
+    end
+  else
+  begin
+    Newrec := TSuperObject.Create;
+    result.AsArray.Add(newrec);
+    for col := 0 to header.AsArray.Length-1 do
+      newrec.S[header.AsArray.S[col]] := '';
+  end;
+end;
+
+function ClipboardSOData: ISuperObject;
+var
+  NewData, row: ISuperObject;
+  St: TStringStream;
+begin
+  Result := TSuperObject.Create(stArray);
+  if Clipboard.HasFormat(ClipbrdJson) then
+  try
+    st := TStringStream.Create('');
+    if Clipboard.GetFormat(ClipbrdJson, St) then
+    begin
+      St.Seek(0, 0);
+      NewData := SO(St.DataString);
+      if NewData.DataType = stArray then
+        for row in NewData do
+          Result.AsArray.Add(row);
+    end;
+  finally
+    St.Free;
+  end;
+end;
+
+
+begin
+  ClipbrdJson := RegisterClipboardFormat('application/json');
 
 end.
 
