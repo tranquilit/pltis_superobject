b862a6bc792670c9c29cc2d5977f0fe558186699
Hubert Touvet <htouvet@tranquil.it>
2013-12-02 17:32:40 +0000

    déplacement fonction ClipboardSOData
    Subversion-branch: /trunk
    Subversion-revision: 14

--- soutils.pas
+++ soutils.pas
@@ -5,7 +5,7 @@ unit soutils;
 interface
 
 uses
-  Classes, SysUtils,SuperObject,DB;
+  Classes, SysUtils,SuperObject,DB,clipbrd,lcltype;
 
 function StringList2SuperObject(St:TStringList):ISuperObject;
 function SplitLines(const St:String):ISuperObject;
@@ -19,6 +19,11 @@ function StrToken(var S: string; Separator: Char): string;
 
 function Dataset2SO(DS:TDataset;AllRecords:Boolean=True):ISuperObject;
 procedure SO2Dataset(SO:ISuperObject;DS:TDataset;ExcludedFields:Array of String);
+
+function ClipboardSOData: ISuperObject;
+
+var
+  ClipbrdJson: TClipboardFormat;
 
 implementation
 uses StrUtils,character,superdate;
@@ -231,7 +236,31 @@ begin
   end;
 end;
 
+function ClipboardSOData: ISuperObject;
+var
+  NewData, row: ISuperObject;
+  St: TStringStream;
+begin
+  Result := TSuperObject.Create(stArray);
+  if Clipboard.HasFormat(ClipbrdJson) then
+  try
+    st := TStringStream.Create('');
+    if Clipboard.GetFormat(ClipbrdJson, St) then
+    begin
+      St.Seek(0, 0);
+      NewData := SO(St.DataString);
+      if NewData.DataType = stArray then
+        for row in NewData do
+          Result.AsArray.Add(row);
+    end;
+  finally
+    St.Free;
+  end;
+end;
 
+
+begin
+  ClipbrdJson := RegisterClipboardFormat('application/json');
 
 end.
 
